6741154a9f7203eb46008087ffe3c76aec3d7918
Noah Waxman <noah@Noahs-Mac-mini.local>
2026-07-29 19:38:39 -0700

    Initial commit

---
+++ database/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE roles (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_roles (
+    user_id UUID,
+    role_id UUID,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
